e4d81eebc639ca9c394d9dfa6fccd47d0e613909
Kartik Kewalramani <113783288+kartik-k3@users.noreply.github.com>
2024-04-21 21:15:48 +0530

    Initial Commit

---
+++ src/firebase/index.js
@@ -0,0 +1,28 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  sendPasswordResetEmail,
+} from "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyBCSLfIIi0F9novIsRR7L1QWFdnoWccHBE",
+  authDomain: "vibe-room-ui.firebaseapp.com",
+  projectId: "vibe-room-ui",
+  storageBucket: "vibe-room-ui.appspot.com",
+  messagingSenderId: "1033636199672",
+  appId: "1:1033636199672:web:e19e2bb3c19e02b7890f0d",
+  measurementId: "G-TEMWVXMGL2",
+};
+
+const app = initializeApp(config);
+const auth = getAuth();
+
+export {
+  app,
+  auth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  sendPasswordResetEmail,
+};
